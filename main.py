db90799f84b150ad9cd18c4719b45d433ca11c41
Maham <maham.r.tanveer@gmail.com>
2021-11-22 07:01:08 +0500

    *

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import numpy as np
-
+from config import get_config
 
 def main():
+    #load configs here
+    config = get_config()
+
     #load the data
-    
+
+
 
 if __name__ == '__main__':
     main()
