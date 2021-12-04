221eb065265870ea72bb8850b9064f01442b6b42
mta122 <mta122@cs-gruvi-40.cmpt.sfu.ca>
2021-12-03 16:01:05 -0800

    tmaham: only retain imnet

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,12 +1,6 @@
 from agents.agent_partae import AgentPartAE
-from agents.agent_seq2seq import Seq2SeqAgent
 
 
 # get relevant agent for use
 def get_agent(name, config):
-    if name == "partae":
-        return AgentPartAE(config)
-    elif config.module == 'seq2seq':
-        return Seq2SeqAgent(config)
-    else:
-        return False
+    return AgentPartAE(config)
